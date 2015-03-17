be3a6396841202c5f5a3099c05c006b5d5b1aa41
Adam Krone <krone.adam@gmail.com>
2015-03-17 16:43:05 -0500

    Require classes in lib/portlet_generator.rb

--- lib/portlet_generator.rb
+++ lib/portlet_generator.rb
@@ -1,5 +1,13 @@
-require "portlet_generator/version"
+require 'portlet_generator/controller'
+require 'portlet_generator/generator'
+require 'portlet_generator/language_file'
+require 'portlet_generator/liferay_portlet_xml'
+require 'portlet_generator/node_injector'
+require 'portlet_generator/portlet'
+require 'portlet_generator/portlet_id_context'
+require 'portlet_generator/portlet_id_portlet'
+require 'portlet_generator/portlet_xml'
+require 'portlet_generator/version'
 
 module PortletGenerator
-  # Your code goes here...
 end
